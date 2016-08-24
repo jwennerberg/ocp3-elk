a1cf99e1163ac235017eddd39973859278a62e67
Jonas Nordell <(none)>
2016-08-24 10:33:50 +0200

    gone fishing

--- logstash/start_logstash.sh
+++ logstash/start_logstash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec /opt/logstash/bin/logstash agent --verbose -f /opt/app-root/logstash.yml  >> /var/tmp/logstash.log 2>&1
+#exec /opt/logstash/bin/logstash agent --verbose -f /opt/app-root/logstash.yml  >> /var/tmp/logstash.log 2>&1
+exec /opt/logstash/bin/logstash agent --verbose -f /opt/app-root/logstash.yml 
